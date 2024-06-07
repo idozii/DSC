071c74b1e25f4f14701be2c033500229c0b00cb0
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2024-06-07 17:06:57 +0700

    fix all yessir

--- bellman.cpp
+++ bellman.cpp
@@ -5,7 +5,6 @@ class TestStudy;
 
 void BellmanFord::BF(int graph[20][20], int num, char Start, int *Value, int *Previous){
      //*Explain: this function returns ONLY 1 step of BF function
-     /*write function to calculate BF step by step */
      bool check = Value[0] != -1 ? true : false;
      if(check){
           int newValue[20];
@@ -14,8 +13,8 @@ void BellmanFord::BF(int graph[20][20], int num, char Start, int *Value, int *Pr
           }
           for(int i = 0; i < num; i++){
                for(int j = 0; j < num; j++){
-                    if(graph[i][j] != 0){
-                         if(newValue[i] + graph[i][j] < Value[j] || (Value[i]==-1 && newValue[i]==-1)){
+                    if(graph[i][j] != 0 && newValue[i] != -1){
+                         if(newValue[i] + graph[i][j] < Value[j] || (Value[j]==-1 && newValue[j]==-1)){
                               Value[j] = newValue[i] + graph[i][j];
                               Previous[j] = i;
                          }
@@ -82,28 +81,3 @@ string BellmanFord::BF_Path(int graph[20][20], int num, char Start, char Goal){
      }
      return path;
 };
-
-//! Fix BF function
-
-int main(){
-     int graph[20][20] = {
-    {0, 34, 58, 99, 61, 21, 57, 63, 9, 65, 28, 75, 8, 79, 61, 64},
-    {68, 0, 79, 11, 60, 42, 96, 26, 92, 98, 50, 1, 61, 62, 96, 83},
-    {64, 97, 0, 6, 62, 57, 27, 85, 86, 37, 49, 14, 78, 23, 93, 4},
-    {88, 27, 83, 0, 65, 53, 92, 61, 80, 49, 24, 29, 50, 85, 58, 12},
-    {33, 88, 8, 39, 0, 16, 31, 67, 0, 16, 3, 16, 97, 47, 39, 56},
-    {18, 93, 83, 67, 24, 0, 35, 58, 51, 81, 7, 75, 10, 57, 25, 68},
-    {35, 24, 55, 9, 63, 71, 0, 40, 29, 38, 23, 99, 54, 86, 12, 59},
-    {41, 30, 52, 90, 97, 76, 25, 0, 55, 93, 5, 62, 33, 82, 85, 24},
-    {15, 20, 48, 37, 29, 10, 74, 36, 0, 6, 11, 25, 71, 31, 77, 83},
-    {91, 18, 12, 42, 74, 76, 84, 65, 30, 0, 42, 37, 58, 75, 18, 8},
-    {99, 0, 95, 12, 3, 91, 89, 77, 93, 95, 0, 55, 84, 66, 86, 60},
-    {14, 42, 44, 27, 84, 18, 69, 33, 83, 65, 75, 0, 19, 22, 15, 4},
-    {97, 13, 71, 92, 92, 74, 82, 81, 16, 74, 75, 71, 0, 57, 6, 23},
-    {83, 21, 65, 27, 14, 15, 11, 83, 14, 94, 47, 90, 80, 0, 35, 4},
-    {50, 31, 85, 20, 22, 76, 94, 70, 56, 9, 9, 98, 47, 66, 0, 70},
-    {70, 15, 91, 0, 42, 71, 15, 53, 53, 30, 12, 66, 86, 92, 0, 0}
-     };
-     BellmanFord bf;
-     cout<<bf.BF_Path(graph, 20, 'A', 'D')<<endl;
-}
